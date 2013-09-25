2306478f67a93e27dd9d7d397f97e3641df3516a
David Shepherd <davidshepherd7@gmail.com>
2013-09-25 23:49:48 +0100

    Add lots of useful default imports to ipython

--- ipython_startup.py
+++ ipython_startup.py
@@ -1,3 +1,24 @@
+from __future__ import division
+from __future__ import absolute_import
+
 import scipy as sp
+import itertools as it
+import functools as ft
+import operator as op
+import sys
+import sympy
+
+
+# Plotting
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.pyplot import subplots
+from matplotlib.pyplot import show as pltshow
+
+
+# and import some common functions into the global namespace
+from scipy.linalg import norm
+from scipy import sin, cos, tan, log, pi, sqrt, exp, mean
+from math import atan2, acos
+from sympy import Rational as sRat
+from sympy import pretty as spretty
