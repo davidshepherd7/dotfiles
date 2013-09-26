0fdaa0027355f03c297a4ba58e22dfc63940c6bd
David Shepherd <davidshepherd7@gmail.com>
2013-09-26 16:55:50 +0100

    Don't generate any character instead of generating the unshifted version

--- keyboard_force_correct_hands.sh
+++ keyboard_force_correct_hands.sh
@@ -9,69 +9,76 @@ setxkbmap gb
 # Set right shift to be a temporary caps lock (ony on while held)
 xmodmap -e "keysym Shift_R = Mode_switch"
 
-
-# Buttons not really on either side, leave these alone so either works.
+# Buttons not really on either side, leave these alone so either shift works:
 # 6 asciicircum
 # y Y
 # b B
+
+
+# Set this to a keysym which is not used elsewhere in your key map so that
+# the wrong key generates nothing. This keysym should be fine for most
+# people. Note: this may interfere with emacs in especially annoying ways
+# because emacs like to tell you when you press unbound keys.
+UBKS="0x3571"
 
 
 # Bind lhs of keyboard to only have its case affected by by this "temporary
 # caps lock modifier".
 
 # letters
-xmodmap -e "keysym q = q q Q Q"
-xmodmap -e "keysym w = w w W W"
-xmodmap -e "keysym e = e e E E"
-xmodmap -e "keysym r = r r R R"
-xmodmap -e "keysym t = t t T T"
-xmodmap -e "keysym a = a a A A"
-xmodmap -e "keysym s = s s S S"
-xmodmap -e "keysym d = d d D D"
-xmodmap -e "keysym f = f f F F"
-xmodmap -e "keysym g = g g G G"
-xmodmap -e "keysym z = z z Z Z"
-xmodmap -e "keysym x = x x X X"
-xmodmap -e "keysym c = c c C C"
-xmodmap -e "keysym v = v v V V"
+xmodmap -e "keysym q = q $UBKS Q Q"
+xmodmap -e "keysym w = w $UBKS W W"
+xmodmap -e "keysym e = e $UBKS E E"
+xmodmap -e "keysym r = r $UBKS R R"
+xmodmap -e "keysym t = t $UBKS T T"
+xmodmap -e "keysym a = a $UBKS A A"
+xmodmap -e "keysym s = s $UBKS S S"
+xmodmap -e "keysym d = d $UBKS D D"
+xmodmap -e "keysym f = f $UBKS F F"
+xmodmap -e "keysym g = g $UBKS G G"
+xmodmap -e "keysym z = z $UBKS Z Z"
+xmodmap -e "keysym x = x $UBKS X X"
+xmodmap -e "keysym c = c $UBKS C C"
+xmodmap -e "keysym v = v $UBKS V V"
 
 # symbols
-xmodmap -e "keysym backslash = backslash backslash bar bar"
-xmodmap -e "keysym grave = grave grave notsign notsign"
-xmodmap -e "keysym 1 = 1 1 exclam"
-xmodmap -e "keysym 2 = 2 2 quotedbl"
-xmodmap -e "keysym 3 = 3 3 sterling"
-xmodmap -e "keysym 4 = 4 4 dollar"
-xmodmap -e "keysym 5 = 5 5 percent"
+xmodmap -e "keysym backslash = backslash $UBKS bar bar"
+xmodmap -e "keysym grave = grave $UBKS notsign notsign"
+xmodmap -e "keysym 1 = 1 $UBKS exclam"
+xmodmap -e "keysym 2 = 2 $UBKS quotedbl"
+xmodmap -e "keysym 3 = 3 $UBKS sterling"
+xmodmap -e "keysym 4 = 4 $UBKS dollar"
+xmodmap -e "keysym 5 = 5 $UBKS percent"
 
 
 
 # Bind rhs of keyboard to only have its case affected by normal shift (not
 # caps lock).
 
-xmodmap -e "keysym u = u U u U"
-xmodmap -e "keysym i = i I i I"
-xmodmap -e "keysym o = o O o O"
-xmodmap -e "keysym p = p P p P"
-xmodmap -e "keysym h = h H h H"
-xmodmap -e "keysym j = j J j J"
-xmodmap -e "keysym k = k K k K"
-xmodmap -e "keysym l = l L l L"
-xmodmap -e "keysym n = n N n N"
-xmodmap -e "keysym m = m M m M"
+# letters
+xmodmap -e "keysym u = u U $UBKS U"
+xmodmap -e "keysym i = i I $UBKS I"
+xmodmap -e "keysym o = o O $UBKS O"
+xmodmap -e "keysym p = p P $UBKS P"
+xmodmap -e "keysym h = h H $UBKS H"
+xmodmap -e "keysym j = j J $UBKS J"
+xmodmap -e "keysym k = k K $UBKS K"
+xmodmap -e "keysym l = l L $UBKS L"
+xmodmap -e "keysym n = n N $UBKS N"
+xmodmap -e "keysym m = m M $UBKS M"
 
 # symbols
-xmodmap -e "keysym 7 = 7 ampersand 7 ampersand"
-xmodmap -e "keysym 8 = 8 asterisk 8 asterisk"
-xmodmap -e "keysym 9 = 9 parenleft 9 parenleft"
-xmodmap -e "keysym 0 = 0 parenright 0 parenright"
-xmodmap -e "keysym minus = minus underscore minus underscore"
-xmodmap -e "keysym equal = equal plus equal plus"
-xmodmap -e "keysym bracketleft = bracketleft braceleft bracketleft braceleft"
-xmodmap -e "keysym bracketright = bracketright braceright bracketright braceright"
-xmodmap -e "keysym semicolon = semicolon colon semicolon colon"
-xmodmap -e "keysym apostraphe = apostraphe at apostraphe at"
-xmodmap -e "keysym numbersign = numbersign asciitilde numbersign asciitilde"
-xmodmap -e "keysym comma = comma less comma less"
-xmodmap -e "keysym period = period greater period greater"
-xmodmap -e "keysym slash = slash question slash question"
+xmodmap -e "keysym 7 = 7 ampersand $UBKS ampersand"
+xmodmap -e "keysym 8 = 8 asterisk $UBKS asterisk"
+xmodmap -e "keysym 9 = 9 parenleft $UBKS parenleft"
+xmodmap -e "keysym 0 = 0 parenright $UBKS parenright"
+xmodmap -e "keysym minus = minus underscore $UBKS underscore"
+xmodmap -e "keysym equal = equal plus $UBKS plus"
+xmodmap -e "keysym bracketleft = bracketleft braceleft $UBKS braceleft"
+xmodmap -e "keysym bracketright = bracketright braceright $UBKS braceright"
+xmodmap -e "keysym semicolon = semicolon colon $UBKS colon"
+xmodmap -e "keysym apostrophe = apostrophe at $UBKS at"
+xmodmap -e "keysym numbersign = numbersign asciitilde $UBKS asciitilde"
+xmodmap -e "keysym comma = comma less $UBKS less"
+xmodmap -e "keysym period = period greater $UBKS greater"
+xmodmap -e "keysym slash = slash question $UBKS question"
