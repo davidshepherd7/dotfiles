d617fed8e55d72d955a8c894a174c1b5646eb589
David Shepherd <davidshepherd7@gmail.com>
2014-12-03 16:03:55 +0000

    Add switch-to/from test file in java

--- .emacs.d/lisp/java.el
+++ .emacs.d/lisp/java.el
@@ -17,11 +17,60 @@
 ;; (require 'ac-emacs-eclim-source)
 ;; (ac-emacs-eclim-config)
 
+(require 's)
+(require 'dash)
+
+(defun java-toggle-test-file ()
+  "Try to locate the file containing tests for this file (or vice versa)."
+  (interactive)
+  (if (s-contains? "test" (buffer-file-name) 't)
+      (java-switch-from-test)
+    (java-switch-to-test)))
+
+(defun java-switch-to-test ()
+  (let* ((bf-dir (file-name-directory (buffer-file-name)))
+         (bf-ext (s-concat "." (file-name-extension (buffer-file-name))))
+         (bf-fnm (file-name-base (buffer-file-name)))
+
+         (files-to-try
+          (list (s-concat bf-fnm "Test" bf-ext)
+                (s-concat "Test" bf-fnm bf-ext)))
+
+         (dirs-to-try
+          (-distinct (list bf-dir
+                           (s-replace "src/" "test/" bf-dir)
+                           (s-replace "source/" "test/" bf-dir))))
+
+         (paths-to-try (-table-flat #'s-concat dirs-to-try files-to-try)))
+
+    ;; Open the first file that exists
+    (find-file (car (-filter #'file-exists-p paths-to-try)))))
+
+(defun java-switch-from-test ()
+  (let* ((bf-dir (file-name-directory (buffer-file-name)))
+         (bf-ext (s-concat "." (file-name-extension (buffer-file-name))))
+         (bf-fnm (file-name-base (buffer-file-name)))
+
+         (files-to-try
+          (list (s-replace "Test" "" (s-concat bf-fnm bf-ext))))
+
+         (dirs-to-try
+          (-distinct (list bf-dir
+                           (s-replace "test/" "src/" bf-dir)
+                           (s-replace "test/" "source/" bf-dir))))
+
+         (paths-to-try (-table-flat #'s-concat dirs-to-try files-to-try)))
+
+    ;; Open the first file that exists
+    (find-file (car (-filter #'file-exists-p paths-to-try)))))
+
+
 (defun java-keys ()
   (interactive)
   ;; (local-set-key (kbd "M-.") 'eclim-java-find-declaration)
-  
+
   (local-set-key (kbd "C-c") '())
+  (local-set-key (kbd "C-\\ o") #'java-toggle-test-file)
 
   ;; ;; Fix tab bindings (messed up by yas and/or binding C-i
   ;; (local-set-key (kbd "TAB") '())
