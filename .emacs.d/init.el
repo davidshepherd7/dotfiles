82b79bf49fb2029b75d47922268e545b987086e3
David Shepherd <davidshepherd7@gmail.com>
2014-12-08 09:48:48 +0000

    Add automatic creation of directories that don't exist
    from: http://iqbalansari.github.io/blog/2014/12/07/automatically-create-parent-directories-on-visiting-a-new-file-in-emacs/

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -167,6 +167,16 @@
 ;; nice with scheme).
 (setq-default require-final-newline 0)
 
+
+;; Create non-existant directories automatically
+(defun my-create-non-existent-directory ()
+  (let ((parent-directory (file-name-directory buffer-file-name)))
+    (when (and (not (file-exists-p parent-directory))
+               (y-or-n-p (format "Directory `%s' does not exist! Create it?" parent-directory)))
+      (make-directory parent-directory t))))
+(add-to-list 'find-file-not-found-functions #'my-create-non-existent-directory)
+
+
 ;; Use frames instead of emacs "windows"
 ;; ============================================================
 (load-file "~/.emacs.d/frames-only-mode/frames-only-mode.el")
