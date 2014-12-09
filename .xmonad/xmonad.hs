28225bb4d8e09c51bffe3e737cf6fd3d7ae969e0
David Shepherd <davidshepherd7@gmail.com>
2014-12-09 18:07:39 +0000

    Use sxhkd for hotkeys instead of xmonad
    * Closer to shell, so slightly simpler (e.g. emacs daemon "just works").
    
    * Allows changing of keys without recompile of WM, and vice-versa.
    
    * If I ever want to change WM, don't have to change hotkeys

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -34,54 +34,6 @@ import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.EwmhDesktops
 
 ------------------------------------------------------------------------
--- Applications
-
--- If possible just launch a new emacs client frame with the most recent
--- not currently shown buffer. If not possible then probably the emacs
--- daemon is not running. So run a new emacs daemon (via zsh so that it
--- inherits all the shell variables from .zshrc), then launch an
--- emacsclient frame.
-myEditor = "emacsclient -c -n -e '(switch-to-buffer nil)' || (zsh -l -c \"emacs --daemon\" && emacsclient -c -n )"
-
--- App for taking notes, use deft in emacs
-myNotes = "emacsclient -c -n -e '(new-clean-deft)'"
-
--- Try various terminals till one works.
-myTerminal = "urxvt -e zsh || xfce4-terminal -e zsh || gnome-terminal -e zsh"
-
--- Terminal running fish shell
-myFish = "urxvt -e fish || gnome-terminal -e fish"
-
--- Terminal shortcuts, don't really use non-urxvt terminals anymore so
--- can't be bothered with the || stuff...
-oomphTerminal = "urxvt -e /bin/sh -c 'cd ~/oomph-lib && /bin/zsh'"
-
-micromagTerminal = "urxvt -e /bin/sh -c 'cd ~/oomph-lib/user_drivers/micromagnetics && /bin/zsh'"
-driverTerminal = "urxvt -e /bin/sh -c 'cd ~/oomph-lib/user_drivers/micromagnetics/control_scripts/driver && /bin/zsh'"
-optDriverTerminal = "urxvt -e /bin/sh -c 'cd ~/optoomph/user_drivers/micromagnetics/control_scripts && /bin/zsh'"
-
-cwsTerminal = "urxvt -e /bin/sh -c 'cd ~/workflows/cloudworkflowsimulator && /bin/zsh'"
-
--- Try various browsers until one works.
-myBrowser = "firefox || google-chrome || chromium || chromium-browser"
-
--- Run dmenu for executables with whatever other stuff I've set up.
--- myLauncher =  "~/.xmonad/dmenu/my-dmenu-run.sh"
-myLauncher = "dmenu_run"
-
--- Run dmenu for folders and open in whatever gui file manager we have.
-guiFolderOpen = "~/.xmonad/dmenu/dfoldermenu.sh"
-
--- Try various locking mechanisms until one works.
-myLockScreen = "mate-screensaver-command -l || xflock4 || gnome-screensaver-command -l"
-
-myiPythonCommand = "zsh -l -c \"ipython3\""
-myiPython = "urxvt -e " ++ myiPythonCommand ++
-            " || gnome-terminal -e " ++ myiPythonCommand
-
-myGhci = "urxvt -e ghci"
-
-------------------------------------------------------------------------
 -- Window rules:
 
 -- Execute arbitrary actions and WindowSet manipulations when managing
@@ -197,8 +149,7 @@ myWorkspaces = ["1","2","3","plots",
 main = do
      -- xmproc <- spawnPipe "xmobar ~/.xmobarrc"
      xmonad $ gnomeConfig
-        { terminal   = myTerminal
-        , modMask    = mod4Mask -- use super as modifier key
+        { modMask    = mod4Mask -- use super as modifier key
         , borderWidth = 2
         , manageHook = myManageHook
         , workspaces = myWorkspaces
@@ -212,7 +163,7 @@ main = do
         , handleEventHook = myEventHook
         }
         -- Unbind some keys
-        `removeKeysP` ["M-S-q" ,"M-e", "M-r", "M-S-e", "M-S-r", "M-.", "M-,"]
+        `removeKeysP` ["M-S-q" ,"M-e", "M-r", "M-S-e", "M-S-r", "M-.", "M-,", "M-t", "M-m", "M-p"]
         `additionalKeysP` myKeys
 
 -- Some additional keybinds, mostly inspired by chromes tab management
@@ -225,58 +176,39 @@ myKeys = [
   ("M-w", kill)
   , ("M-S-w", spawn "xkill")
 
-    -- run things
-  , ("M-t", spawn myTerminal)
-  -- , ("M-f", spawn myFish)
-  , ("M-f", spawn myEditor)
-  , ("M-p", spawn myLauncher)
-  , ("M-r", spawn oomphTerminal)
-  , ("M-s", spawn cwsTerminal)
-  , ("M-d", spawn driverTerminal)
-  , ("M-a", spawn optDriverTerminal)
-  , ("M-m", spawn myNotes)
 
-  , ("M-y", spawn myBrowser)
-  , ("M-,", spawn myiPython)
-  , ("M-.", spawn myGhci)
-
-    
-    
-    -- movement
+  -- movement
   , ("M-<R>", sendMessage Expand)
   , ("M-<L>", sendMessage Shrink)
   , ("M-e", windows W.focusUp)
 
 
-    -- multiple screens
+  -- multiple screens
   , ("M-x", nextScreen)
   , ("M-S-x", shiftNextScreen)
   , ("M-z", swapNextScreen)
 
-    -- Lock screen
-  , ("M-=", spawn myLockScreen)
-
-    -- Quit xmonad (e.g. if we have no gnome session this is the only way to log out)
+  -- Quit xmonad (e.g. if we have no gnome session this is the only way to log out)
   , ("M-S-q", io (exitWith ExitSuccess))
 
-    -- go to previous workspace
+  -- go to previous workspace
   , ("M-<Backspace>", toggleWS)
 
-    -- Next/previous workspace
+  -- Next/previous workspace
   , ("M-i", nextWS)
   , ("M-n", prevWS)
   , ("M-S-i", shiftToNext)
   , ("M-S-n", shiftToPrev)
 
 
-    -- Floats
+  -- Floats
   , ("M-u", withFocused $ windows . W.sink)
   ]
-         ++ -- ++ combines the two lists
+  ++ -- ++ combines the two lists
 
-         -- Create a list of bindings:
-         -- set all "M-number" to W.view that number
-         [ (otherModMasks ++ "M-" ++ [key], action tag)
-         | (tag, key)  <- zip myWorkspaces "123456789"
-         , (otherModMasks, action) <- [ ("", windows . W.view)
-                                      , ("S-", windows . W.shift)]]
+     -- Create a list of bindings:
+     -- set all "M-number" to W.view that number
+     [ (otherModMasks ++ "M-" ++ [key], action tag)
+       | (tag, key)  <- zip myWorkspaces "123456789"
+       , (otherModMasks, action) <- [ ("", windows . W.view)
+       , ("S-", windows . W.shift)]]
