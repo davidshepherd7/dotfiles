a701dada5a7861eb4fb11eca2ed227c10bf2b835
David Shepherd <davidshepherd7@gmail.com>
2015-04-27 10:48:29 +0100

    Update python packages to use in ipython/pip installer

--- ipython_startup.py
+++ ipython_startup.py
@@ -1,13 +1,10 @@
-from __future__ import division
-from __future__ import absolute_import
 
-import scipy as sp
 import itertools as it
 import functools as ft
 import operator as op
 import sys
-# import sympy
 import os
+import os.path
 
 # Plotting
 import matplotlib.pyplot as plt
@@ -15,17 +12,23 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.pyplot import subplots
 from matplotlib.pyplot import show as pltshow
 
-
 # and import some common functions into the global namespace
 from scipy.linalg import norm, eig
 from scipy import sin, cos, tan, log, pi, sqrt, exp, mean
 from math import atan2, acos
-# from sympy import Rational as rat
-# from sympy import pretty as spretty
 
-import oomphpy
-import oomphpy.micromagnetics as mm
-import oomphpy.tests
-import oomphpy.matrices
-from oomphpy.matrices import ascii2coo
-from oomphpy.matrices import ascii2array
+import sympy
+from sympy import Rational as rat
+from sympy import pretty as spretty
+
+import scipy as sp
+import numpy as np
+
+import cytoolz as tlz
+
+# import oomphpy
+# import oomphpy.micromagnetics as mm
+# import oomphpy.tests
+# import oomphpy.matrices
+# from oomphpy.matrices import ascii2coo
+# from oomphpy.matrices import ascii2array
