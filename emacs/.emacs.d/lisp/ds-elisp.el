1ece92551b5b628f819a1646601acfbd947012a0
David Shepherd <davidshepherd7@gmail.com>
2015-06-04 22:56:37 +0100

    Add function to insert `kbd` commands in elisp

--- emacs/.emacs.d/lisp/ds-elisp.el
+++ emacs/.emacs.d/lisp/ds-elisp.el
@@ -5,3 +5,10 @@
 ;; Use double semi-colon for emacs lisp (default seems to be single).
 (add-hook 'emacs-lisp-mode-hook (lambda () (setq comment-start ";;"
                                             comment-end "")))
+
+
+(defun insert-key-as-kbd (key)
+  (interactive "kKey: ")
+  (insert (format "(kbd \"%s\")" (key-description key))))
+
+(define-key emacs-lisp-mode-map (kbd "C-,") #'insert-key-as-kbd)
