c5799330f1f2666ccedc03df9ab47806c8a07ca8
David Shepherd <davidshepherd7@gmail.com>
2015-06-12 09:34:40 +0100

    Add yaml mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1098,6 +1098,11 @@ $0")
             java-mode-hook))
   )
 
+(use-package yaml-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode)))
+
 (use-package feature-mode
   :ensure t
   :config
