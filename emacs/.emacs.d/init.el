aa46c2c07032d5a7cf7891e5e0c64f7238ca7309
David Shepherd <davidshepherd7@gmail.com>
2015-07-20 12:35:50 +0100

    Add multiple cursors package w/ hydra

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1331,6 +1331,30 @@ $0")
 
   )
 
+(use-package multiple-cursors
+  :ensure t
+  :config
+
+  (require 'hydra)
+  (defhydra hydra-multiple-cursors (:hint nil)
+    "
+     ^Up^            ^Down^        ^Miscellaneous^
+----------------------------------------------
+[_p_]   Next    [_n_]   Next    [_l_] Edit lines
+[_P_]   Skip    [_N_]   Skip    [_a_] Mark all
+[_M-p_] Unmark  [_M-n_] Unmark  [_q_] Quit"
+
+  ("l" mc/edit-lines :exit t)
+  ("a" mc/mark-all-like-this :exit t)
+  ("n" mc/mark-next-like-this)
+  ("N" mc/skip-to-next-like-this)
+  ("M-n" mc/unmark-next-like-this)
+  ("p" mc/mark-previous-like-this)
+  ("P" mc/skip-to-previous-like-this)
+  ("M-p" mc/unmark-previous-like-this)
+  ("q" nil))
+
+  )
 
 ;; Use rainbow delimeters to highlight mismatched parens.
 (use-package rainbow-delimiters
