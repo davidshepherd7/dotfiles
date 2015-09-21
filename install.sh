6d93a2932fde70fbda3817610f13fb9366a78e65
David Shepherd <davidshepherd7@gmail.com>
2015-09-21 17:57:54 +0100

    Deal with stupid .dropbox.attr files

--- install.sh
+++ install.sh
@@ -3,36 +3,36 @@
 set -o errexit
 set -o nounset
 
-stow zsh -t $HOME
+stow zsh -t $HOME --ignore=".dropbox.attr"
 
 mkdir -p ~/.emacs.d
-stow emacs -t $HOME
+stow emacs -t $HOME --ignore=".dropbox.attr"
 
-stow X -t $HOME
+stow X -t $HOME --ignore=".dropbox.attr"
 
 mkdir -p ~/.xmonad
-stow xmonad -t $HOME
+stow xmonad -t $HOME --ignore=".dropbox.attr"
 
-stow conky -t $HOME
+stow conky -t $HOME --ignore=".dropbox.attr"
 
-stow git -t $HOME
+stow git -t $HOME --ignore=".dropbox.attr"
 
 mkdir -p ~/.ipython/profile_default/
 mkdir -p ~/.ipython/profile_default/startup
-stow ipython -t $HOME
+stow ipython -t $HOME --ignore=".dropbox.attr"
 
 mkdir -p ~/.config/sxhkd
-stow sxhkd -t $HOME
+stow sxhkd -t $HOME --ignore=".dropbox.attr"
 
-stow readline -t $HOME
+stow readline -t $HOME --ignore=".dropbox.attr"
 
-stow gdb -t $HOME
+stow gdb -t $HOME --ignore=".dropbox.attr"
 
-stow bash -t $HOME
+stow bash -t $HOME --ignore=".dropbox.attr"
 
 # Note: requires sudo
 sudo mkdir -p /etc/udev/hwdb.d
-sudo stow hwdb -t /etc/udev/hwdb.d
+sudo stow hwdb -t /etc/udev/hwdb.d --ignore=".dropbox.attr"
 
 # Just do a plain copy for this, links don't seem to work
 sudo cp desktop-session/xsession.desktop /usr/share/xsessions/
