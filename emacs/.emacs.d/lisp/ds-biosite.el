4db0285d7d06a90b32cad1e71062f7f8749cd7d6
David Shepherd <davidshepherd7@gmail.com>
2015-09-29 09:25:25 +0100

    Biosite stuff

--- emacs/.emacs.d/lisp/ds-biosite.el
+++ emacs/.emacs.d/lisp/ds-biosite.el
@@ -8,9 +8,9 @@
 
   (c-set-offset 'substatement-open '0)
 
-  (c-set-offset 'arglist-cont-nonempty '++)
-  (c-set-offset 'arglist-intro '++)
-  (c-set-offset 'arglist-close '++))
+  (c-set-offset 'arglist-cont-nonempty '+)
+  (c-set-offset 'arglist-intro '+)
+  (c-set-offset 'arglist-close '+))
 
 (defun ds/tabs-mode ()
   (interactive)
@@ -18,3 +18,18 @@
 
 (add-hook 'c++-mode-hook #'biosite-c-style)
 (add-hook 'c++-mode-hook #'ds/tabs-mode)
+
+(defun ds/biosite-html-style ()
+  (interactive)
+  (setq sgml-basic-offset tab-width)
+  (setq indent-tabs-mode t)
+  (setq tab-stop-list '(4 8 12 16)))
+
+(add-hook 'html-mode-hook #'ds/biosite-html-style)
+
+(defun ds/biosite-js-style ()
+  (interactive)
+  (setq indent-tabs-mode t)
+  (setq js-indent-level tab-width)
+  (setq js0comment-)
+  )
