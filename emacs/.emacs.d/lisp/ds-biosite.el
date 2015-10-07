ebcfd1653001c5e3c29522841e957e7ffa36b1b6
David Shepherd <davidshepherd7@gmail.com>
2015-10-07 10:31:05 +0100

    More biosite coding standards stuff

--- emacs/.emacs.d/lisp/ds-biosite.el
+++ emacs/.emacs.d/lisp/ds-biosite.el
@@ -1,7 +1,18 @@
 
+(defun ds/disable-tabs-advice (&rest args)
+  "Use spaces not tabs for advised function"
+  (let ((indent-tabs-mode nil))
+    (apply args)))
+(advice-add #'align :around #'ds/disable-tabs-advice)
+(advice-add #'align-regexp :around #'ds/disable-tabs-advice)
+(advice-add #'align-region :around #'ds/disable-tabs-advice)
+;; Add any other alignment functions here
+
 
 (defun biosite-c-style ()
   (interactive)
+
+  (set 'indent-tabs-mode t)
 
   ;; Ensure that one offset is a tab
   (set 'c-basic-offset tab-width)
@@ -12,12 +23,8 @@
   (c-set-offset 'arglist-intro '+)
   (c-set-offset 'arglist-close '+))
 
-(defun ds/tabs-mode ()
-  (interactive)
-  (set 'indent-tabs-mode t))
-
 (add-hook 'c++-mode-hook #'biosite-c-style)
-(add-hook 'c++-mode-hook #'ds/tabs-mode)
+
 
 (defun ds/biosite-html-style ()
   (interactive)
@@ -27,9 +34,15 @@
 
 (add-hook 'html-mode-hook #'ds/biosite-html-style)
 
+
 (defun ds/biosite-js-style ()
   (interactive)
   (setq indent-tabs-mode t)
-  (setq js-indent-level tab-width)
-  (setq js0comment-)
-  )
+  (setq js-indent-level tab-width))
+(add-hook 'js-mode-hook #'ds/biosite-js-style)
+
+
+(defun ds/biosite-cmake-style ()
+  (interactive)
+  (setq indent-tabs-mode t))
+(add-hook 'cmake-mode-hook #'ds/biosite-cmake-style)
