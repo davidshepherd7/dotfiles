09b1208e467a74a9f5e0e6c382c1b03a3cc681f5
David Shepherd <davidshepherd7@gmail.com>
2015-11-08 20:50:24 +0000

    Add some hg commands

---
+++ bin/bin/pa-pull-request.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+sensible-browser "$(hg paths default)/pull-request/new" &
+
+subject="PR+Boron%3A+"
+to="software%40biositesystems.com"
+body=""
+
+gmail_biosite_account="0"
+
+
+mail_url="https://mail.google.com/mail/u/${gmail_biosite_account}/?view=cm&to=$to&su=$subject&body=$body"
+sensible-browser "$mail_url" &
