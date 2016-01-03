1985502f0e5e489d6d3195c89431439c7876f820
David Shepherd <davidshepherd7@gmail.com>
2016-01-03 17:51:02 +0000

    More biosite tweaks

--- emacs/.emacs.d/lisp/ds-biosite.el
+++ emacs/.emacs.d/lisp/ds-biosite.el
@@ -31,6 +31,9 @@
 
   ;; Indent by single tab for streaming
   (c-set-offset 'stream-op '+)
+
+  ;; Indent comments by one tab
+  (c-set-offset 'c '+)
   )
 
 (add-hook 'c++-mode-hook #'biosite-c-style)
@@ -40,7 +43,7 @@
   (interactive)
   (setq sgml-basic-offset tab-width)
   (setq indent-tabs-mode t)
-  (setq tab-stop-list '(4 8 12 16)))
+  )
 
 (add-hook 'html-mode-hook #'ds/biosite-html-style)
 
@@ -69,11 +72,44 @@
        (replace-regexp-in-string "/tests/" "/" it)
        (replace-regexp-in-string "\.cpp" ".h" it)))
 
+
+(defun ds/align-comma ()
+  (interactive)
+  (align-regexp (region-beginning) (region-end) ",\\(\\s-*\\)"))
+
 ;; align commas (for PA_SERIALISE macros)
 (require 'align)
 (add-to-list 'align-rules-list
              '(align-biosite-comma
-               (regexp . ",\\(\\s-*\\)[^\\s-*]")
-               (group . 1)
-               (modes . '(c-mode c++-mode))
-               (repeat . nil)))
+               (regexp . ",\\(\\s-*\\)")))
+
+
+               (defun ds/biosite-sql-style ()
+                 (interactive)
+                 (set 'indent-tabs-mode t))
+(add-hook 'sql-mode-hook #'ds/biosite-sql-style)
+
+
+
+(defun ds/new-boron-sql ()
+  (interactive)
+  (let (
+        (next-version (-->
+                       (f-glob (f-join (projectile-project-root) "boron/shared/db/sql/upgrade_*_to_*.sql"))
+                       (-sort (lambda (s1 s2) (not (string-lessp s1 s2))) it)
+                       (car it)
+                       (f-filename it)
+                       (f-base it)
+                       (s-split "_" it)
+                       (nth 3 it)
+                       (string-to-number it)
+                       (+ 1 it)
+                       ))
+        )
+    (-->
+     (format "upgrade_%03i_to_%03i.sql" (- next-version 1) next-version)
+     (f-join (projectile-project-root) "boron/shared/db/sql" it)
+     (find-file it))
+
+    (insert (format "UPDATE version SET current = %i;" next-version))
+    ))
