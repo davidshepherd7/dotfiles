acf0ddbabe54d849e0ef52a4382b0a7ac68472f7
David Shepherd <davidshepherd7@gmail.com>
2016-05-03 20:28:43 +0100

    Add dmenu fallback to helm-dmenu

---
+++ bin/bin/helm-dmenu-alternative.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Run dmenu_run ignoring all arguments
+dmenu_run
--- bin/bin/helm-dmenu.sh
+++ bin/bin/helm-dmenu.sh
@@ -7,4 +7,5 @@ emacsclient \
     -e "(set-frame-parameter (selected-frame) 'title \"helm-dmenu\")" \
     -e "(let ((helm-full-frame t)) (call-interactively #'helm-run-external-command))" \
     -e "(delete-frame)" \
-    -c
+    -c \
+    -a "helm-dmenu-alternative.sh"
