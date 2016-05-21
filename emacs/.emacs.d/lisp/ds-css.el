0aca6ae9ebbac13b491428b415512f25e90838e1
David Shepherd <davidshepherd7@gmail.com>
2016-05-21 17:25:01 +0100

    Fix tab key in css mode

--- emacs/.emacs.d/lisp/ds-css.el
+++ emacs/.emacs.d/lisp/ds-css.el
@@ -1,6 +1,6 @@
 
 (require 'css-mode)
 
-(add-hook 'css-mode-hook #'set-tab)
+(add-hook 'css-mode-hook #'set-tab t)
 (add-hook 'css-mode-hook #'electric-operator-mode)
 (add-hook 'css-mode-hook #'aggressive-indent-mode)
