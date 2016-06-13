b48f177437d5823d103d28b0e20603b3badeda86
David Shepherd <davidshepherd7@gmail.com>
2016-06-13 20:41:31 +0100

    Actually load css emacs config file!

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -539,6 +539,7 @@ index in STRING."
 (load-file "~/.emacs.d/lisp/ds-sql.el")
 (load-file  "~/.emacs.d/lisp/ds-cmake.el")
 (load-file  "~/.emacs.d/lisp/ds-sgml.el")
+(load-file  "~/.emacs.d/lisp/ds-css.el")
 (load-file  "~/.emacs.d/lisp/ds-sh.el")
 
 (load-file "~/.emacs.d/lisp/ds-oomph-lib.el")
