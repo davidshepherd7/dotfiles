2987373e5a4603448047013aa6d5158f33576086
David Shepherd <davidshepherd7@gmail.com>
2016-07-29 12:07:22 +0100

    Don't nuke subrepos with hg clean whitespace

--- bin/bin/hg-clean-whitespace.sh
+++ bin/bin/hg-clean-whitespace.sh
@@ -5,8 +5,20 @@ set -o nounset
 
 tempfile="$(mktemp "hg-clean-whitespace-$USER.XXXXXXX" --tmpdir)"
 backupfile="$(mktemp "hg-clean-whitespace-backup-$USER.XXXXXXX" --tmpdir)"
+
 # Disable user customisations
 export HGPLAIN="on"
+
+substatus="$(cd "$(hg root)" && awk '{print $2}' .hgsubstate | xargs -n1 -I{} sh -c 'cd {} && hg status --')"
+
+if [ "$substatus" != "" ]; then
+    cat <<EOF >&2
+Sub repos not clean:
+    $substatus
+Aborting!
+EOF
+    exit 5
+fi
 
 # Backup a diff
 hg diff > "$backupfile"
