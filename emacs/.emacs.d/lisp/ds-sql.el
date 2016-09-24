f6c4bdd6408130eaabd2fc5cac9bd1026286f469
David Shepherd <davidshepherd7@gmail.com>
2016-09-24 21:41:34 +0100

    Add simple sqlup blacklist

--- emacs/.emacs.d/lisp/ds-sql.el
+++ emacs/.emacs.d/lisp/ds-sql.el
@@ -5,6 +5,10 @@
   :ensure t
   :diminish sqlup-mode
   :config
+  (add-to-list 'sqlup-blacklist "user")
+  (add-to-list 'sqlup-blacklist "name")
+  (add-to-list 'sqlup-blacklist "group")
+  (add-to-list 'sqlup-blacklist "data")
   (add-hook 'sql-mode-hook #'sqlup-mode t))
 
 (add-hook 'sql-mode-hook #'flycheck-mode)
