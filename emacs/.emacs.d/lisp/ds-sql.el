547dd4a85ef762871b3a5430810b6715224cc885
David Shepherd <davidshepherd7@gmail.com>
2017-01-28 13:51:30 +0000

    Use sql-indent

--- emacs/.emacs.d/lisp/ds-sql.el
+++ emacs/.emacs.d/lisp/ds-sql.el
@@ -1,5 +1,6 @@
 
 (require 'use-package)
+(require 'sql)
 
 (use-package sqlup-mode
   :ensure t
@@ -10,6 +11,11 @@
   (add-to-list 'sqlup-blacklist "group")
   (add-to-list 'sqlup-blacklist "data")
   (add-hook 'sql-mode-hook #'sqlup-mode t))
+
+(use-package sql-indent
+  :load-path "~/.emacs.d/sql-indent/"
+  :config
+  (add-hook 'sql-mode-hook #'sql-indent-mode))
 
 (add-hook 'sql-mode-hook #'flycheck-mode)
 
