fd151418fd9646cd8d27c35d17dca4200085ccdf
David Shepherd <davidshepherd7@gmail.com>
2017-07-05 16:05:44 +0100

    Add some docker support in vm alias

--- zsh/.zsh/biosite-vms.sh
+++ zsh/.zsh/biosite-vms.sh
@@ -11,6 +11,10 @@ _complete-vms() {
     reply=(`vboxmanage list runningvms | awk '{print $1}' | tr -d '"'`);
 }
 
+_complete-containers() {
+    reply=(`docker ps --format "{{.Names}}"`)
+}
+
 # Get the ip of a local virtualbox vm. The VM must be running, booted into
 # Linux, have the package virtualbox-guest-utils installed, and have been
 # rebooted since that package was installed.
@@ -19,6 +23,12 @@ vm-get-ip() {
     vboxmanage guestproperty get "$server_name" '/VirtualBox/GuestInfo/Net/0/V4/IP' | awk '{print $2}'
 }
 compctl -K _complete-vms vm-get-ip
+
+docker-get-ip() {
+    local server_name="$1"
+    docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$server_name"
+}
+compctl -K _complete-containers docker-get-ip
 
 # As above, but wait until we get an ip
 vm-blocking-get-ip() {
@@ -60,6 +70,16 @@ vm-psql() {
     vm-ssh "$vm_name" env PGPASSWORD="biosite" psql -U boron_user boron -h localhost "$@"
 }
 compctl -K _complete-vms vm-psql
+
+docker-psql() {
+    local docker_name="$1"
+    shift 1
+
+    docker exec -it "$docker_name" \
+           env PGPASSWORD="biosite" \
+           psql -U boron_user boron -h localhost "$@"
+}
+compctl -K _complete-containers docker-psql
 
 # Shutdown, delete, and clear data for a VM
 nuke-vm() {
