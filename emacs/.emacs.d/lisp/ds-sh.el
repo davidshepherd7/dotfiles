22f2343ae45a637b89e98e3f289aad252a92bbed
David Shepherd <davidshepherd7@gmail.com>
2017-12-03 10:30:36 +0000

    Fix flycheck shellcheck

--- emacs/.emacs.d/lisp/ds-sh.el
+++ emacs/.emacs.d/lisp/ds-sh.el
@@ -1,10 +1,14 @@
 
 (require 'shell)
 (require 'sh-script)
+(require 'validate)
 
 (add-hook 'sh-mode-hook 'set-tab)
 
 (require 'flycheck)
 (add-hook 'sh-mode-hook #'flycheck-mode)
+
+;; My shellcheck doesn't support this
+(validate-setq flycheck-shellcheck-follow-sources nil)
 
 (add-hook 'sh-mode-hook #'sh-electric-here-document-mode)
