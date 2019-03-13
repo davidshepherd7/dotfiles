46425711587a3c6636aa6261c7c7fb94a8dd8ea6
David Shepherd <davidshepherd7@gmail.com>
2019-03-13 15:32:52 +0000

    Fix switching from html to ts/js as needed

--- biosite/boron-dir-locals.el
+++ biosite/boron-dir-locals.el
@@ -2,7 +2,6 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil
-  (ds/sgml-switch-extension . ".js")
   (projectile-project-compilation-dir . "./build")
   (projectile-project-compilation-cmd . "ninja")
   (projectile-hg-command . "hg files -0 -S")))
--- emacs/.emacs.d/lisp/ds-sgml.el
+++ emacs/.emacs.d/lisp/ds-sgml.el
@@ -14,18 +14,21 @@
 (require 'aggressive-indent)
 (add-hook 'sgml-mode-hook #'aggressive-indent-mode)
 
-(defcustom ds/sgml-switch-extension ".js"
-  "File extension to switch to")
-
 (defun ds/switch-to-js ()
   (interactive)
-  (ds/switch-to-related ds/sgml-switch-extension))
+  (--> '(".ts" ".js")
+       (--map (concat (file-name-sans-extension (buffer-file-name)) it) it)
+       (--first (f-exists? it) it)
+       (find-file it)))
 (define-key html-mode-map (kbd "C-\\ o") #'ds/switch-to-js)
 
 (defun ds/html-switch-to-test ()
   (interactive)
-  (ds/switch-to-related (concat ".spec" ds/sgml-switch-extension)))
-(define-key html-mode-map (kbd "C-\\ n") #'ds/html-switch-to-test)
+  (--> '(".spec.ts" ".spec.js")
+       (--map (concat (file-name-sans-extension (buffer-file-name)) it) it)
+       (--first (f-exists? it) it)
+       (find-file it)))
+  (define-key html-mode-map (kbd "C-\\ n") #'ds/html-switch-to-test)
 
 
 (define-key sgml-mode-map (kbd "C-M-n") #'sgml-skip-tag-forward)
