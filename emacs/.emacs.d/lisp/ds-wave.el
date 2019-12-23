23e55ea6e433be1d1bf54f6f78ee954ccb5fdbd1
David Shepherd <davidshepherd7@gmail.com>
2019-12-23 11:56:32 +0000

    Add function for running wave tests

--- emacs/.emacs.d/lisp/ds-wave.el
+++ emacs/.emacs.d/lisp/ds-wave.el
@@ -8,3 +8,14 @@
     (conf-windows-mode)))
 (add-hook 'conf-mode-hook #'ds/asterisk-conf-windows)
 
+
+(defun ds/money-srv-test ()
+  "Run tests for a python file in money-srv"
+  (interactive)
+  (let* ((default-directory (or (projectile-compilation-dir) (projectile-project-root)))
+         (file-name (file-relative-name buffer-file-name (f-join (projectile-project-root) "money-srv")))
+         (compilation-read-command t))
+    (when (not (s-contains? "test_" buffer-file-name))
+      (error "This doesn't look like a test file"))
+    (setq compile-command (concat "mm m.typecheck && ./money-srv/bin/run_tests " (shell-quote-argument file-name)))
+    (call-interactively #'compile)))
