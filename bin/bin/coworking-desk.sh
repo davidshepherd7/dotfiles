6736a41bab145aec1ec05f5d9ef807d6193bcb08
David Shepherd <davidshepherd7@gmail.com>
2019-12-23 12:02:43 +0000

    Add screen layout scripts

---
+++ bin/bin/coworking-desk.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --off
+
+wallpaper
