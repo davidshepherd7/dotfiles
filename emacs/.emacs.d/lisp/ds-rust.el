a94cf72c76ed7a51987687ed7ad01d8fb0e4de47
David Shepherd <davidshepherd7@gmail.com>
2019-12-23 11:55:52 +0000

    Clean up some more stuff that never really worked

--- emacs/.emacs.d/lisp/ds-rust.el
+++ emacs/.emacs.d/lisp/ds-rust.el
@@ -4,18 +4,6 @@
 (use-package rust-mode
   :ensure t
   :config
-  (add-hook 'rust-mode-hook #'electric-operator-mode))
+  (add-hook 'rust-mode-hook #'electric-operator-mode)
+  (add-hook 'rust-mode-hook #'lsp-mode))
 
-(use-package racer
-  :ensure t
-  :config
-  (add-hook 'rust-mode-hook #'racer-mode)
-  (add-hook 'racer-mode-hook #'eldoc-mode)
-  (add-hook 'racer-mode-hook #'company-mode)
-
-  (defun ds/set-company-completion-racer-only ()
-    "Make company completions use racer first"
-    (setq-local completion-at-point-functions '(racer-complete-at-point))
-    (setq-local company-backends (cons '(company-capf) company-backends))
-
-    (add-hook 'racer-mode-hook #'ds/set-company-completion-racer-only)))
--- emacs/.emacs.d/lisp/ds-wave.el
+++ emacs/.emacs.d/lisp/ds-wave.el
@@ -1,24 +1,6 @@
 
+(require 's)
 
-;;; Python
-
-(require 'flycheck)
-
-;; (flycheck-def-args-var ds/flycheck-python-dmypy-args python-dmypy)
-
-;; (flycheck-define-checker python-dmypy
-;;   "Dmypy syntax checker.
-;; Customize `flycheck-python-dmypy-args` to add specific args to default
-;; executable.
-;; E.g. when processing Python2 files, add \"--py2\".
-;; See URL `http://mypy-lang.org/'."
-
-;;   :command (ds/flycheck-python-dmypy-binary (eval ds/flycheck-python-dmypy-args))
-;;   :error-patterns
-;;   ((error line-start (file-name) ":" line ":" column ": error:" (message) line-end)
-;;    (warning line-start (file-name) ":" line ":" column ": warning:" (message) line-end)
-;;    (info line-start (file-name) ":" line ":" column ": note:" (message) line-end))
-;;   :modes python-mode)
 
 (defun ds/asterisk-conf-windows ()
   (interactive)
@@ -26,4 +8,3 @@
     (conf-windows-mode)))
 (add-hook 'conf-mode-hook #'ds/asterisk-conf-windows)
 
-;; (add-to-list 'flycheck-checkers 'python-dmypy t)
