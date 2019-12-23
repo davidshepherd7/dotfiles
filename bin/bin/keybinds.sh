df55b5b20fa2e585bc7860fd6c99d7868a833b81
David Shepherd <davidshepherd7@gmail.com>
2019-12-23 11:42:06 +0000

    Don't fail script if mouse not plugged in

--- bin/bin/keybinds.sh
+++ bin/bin/keybinds.sh
@@ -93,9 +93,9 @@ xinput -set-prop "DualPoint Stick" "Device Enabled" 0 &
 
 # Slow dooowwwn the mouse
 xset m 1 0
-xinput --list --short |\
-    grep 'pointer' | grep 'Microsoft' | awk '{print $8}' | cut -d'=' -f 2 |\
-    xargs -n1 -I{} xinput --set-prop {} "Device Accel Constant Deceleration" 2
+(xinput --list --short |\
+     grep 'pointer' | grep 'Microsoft' | awk '{print $8}' | cut -d'=' -f 2 |\
+     xargs -n1 -I{} xinput --set-prop {} "Device Accel Constant Deceleration" 2) || true
 # Bloody microsoft put a trademark symbol in the device name, so we have grep
 # + awk fun times.
 
