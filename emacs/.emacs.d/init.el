5023fa0f4979177383eecf207dbcc8302502cac9
David Shepherd <davidshepherd7@gmail.com>
2019-12-23 11:48:45 +0000

    Add a simple xml pretty printer

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2057,6 +2057,35 @@ for a file to visit if current buffer is not visiting a file."
 (use-package make-mode
   :config
   (validate-setq makefile-mode-map (make-sparse-keymap)))
+
+(defun ds/sgml-pretty-print (beg end)
+  "Simple-minded pretty printer for SGML.
+Re-indents the code and inserts newlines between BEG and END.
+You might want to turn on `auto-fill-mode' to get better results.
+
+Taken from sgml-pretty-print, hacked to not add newlines before end tags."
+  (interactive "r")
+  (save-excursion
+    (if (< beg end)
+        (goto-char beg)
+      (goto-char end)
+      (setq end beg)
+      (setq beg (point)))
+    ;; Don't use narrowing because it screws up auto-indent.
+    (setq end (copy-marker end t))
+    (with-syntax-table sgml-tag-syntax-table
+      (while (re-search-forward "<" end t)
+        (goto-char (match-beginning 0))
+        (unless (or (and (looking-at "</") (looking-back "<[^/>]*>[^<]*"))
+                    (progn (skip-chars-backward " \t") (bolp)))
+          (reindent-then-newline-and-indent))
+        (sgml-forward-sexp 1)))
+    ))
+
+
+(defun ds/sgml-pretty-print-buffer ()
+  (interactive)
+  (ds/sgml-pretty-print (point-min) (point-max)))
 (use-package graphql-mode
   :ensure t
   :config
