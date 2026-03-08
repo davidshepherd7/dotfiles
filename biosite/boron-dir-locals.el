d1cdba40cc72aac097ed8a68b6aeea166f735125
David Nikel-Shepherd <davidshepherd7@gmail.com>
2026-03-08 14:20:24 +0000

    Remove biosite

--- biosite/boron-dir-locals.el
+++
@@ -1,7 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil
-  (projectile-project-compilation-dir . "./build")
-  (projectile-project-compilation-cmd . "ninja")
-  (projectile-hg-command . "hg files -0 -S")))
--- biosite/common-dir-locals.el
+++
@@ -1,6 +0,0 @@
-(
- (nil . ((projectile-project-compilation-dir . "../build")
-         (projectile-project-compilation-cmd . "ninja")
-         )
-      )
- )
